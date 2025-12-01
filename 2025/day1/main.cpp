d48f2441f194deda5900a6ccce31667559cd4106
Wouter Thys <wouter.thys@septentrio.com>
2025-12-01 19:10:00 +0100

    better way

--- 2025/day1/main.cpp
+++ 2025/day1/main.cpp
@@ -52,27 +52,25 @@ void first(vector<Directions> input)
     cout << "Part one: " << count << std::endl;
 }
 
-    // private static int CheckZeroClicks(int startValue, string rotation)
-    // {
-    //     int distance = Convert.ToInt32(rotation[1..]);
+    static int checkZeroClicks(int startValue, Directions d)
+    {
+        // Every 100 in distance gives a full rotation and returns to the start value.
+        int zeroCount = d.amount / 100;
 
-    //     // Every 100 in distance gives a full rotation and returns to the start value.
-    //     int zeroCount = distance / 100;
-
-    //     // The remainder will determine the new end position.
-    //     int remainder = distance % 100;
+        // The remainder will determine the new end position.
+        int remainder = d.amount  % 100;
         
-    //     int newValue = rotation[0] == 'R' ? startValue + remainder : startValue - remainder;
+        int newValue = d.dir == 'R' ? startValue + remainder : startValue - remainder;
 
-    //     if (startValue == 0)
-    //         return zeroCount;
-    //     else if (newValue <= 0)
-    //         zeroCount++;
-    //     else if (newValue >= 100)
-    //         zeroCount++;
+        if (startValue == 0)
+            return zeroCount;
+        else if (newValue <= 0)
+            zeroCount++;
+        else if (newValue >= 100)
+            zeroCount++;
 
-    //     return zeroCount;
-    // }
+        return zeroCount;
+    }
 
 
 void second(vector<Directions> input) 
@@ -83,85 +81,52 @@ void second(vector<Directions> input)
 
     for (const auto& d : input) 
     {
-        for (int i = 0; i < d.amount; i++) 
-        {
-            if (position == 0) 
-            {
-                count++;
-            }
-            if (d.dir == 'R') 
-            {
-                position++;
-                if (position > 99) 
-                {
-                    position = 0;
-                }
-            }
-            else 
-            {
-                position--;
-                if (position < 0) 
-                {
-                    position = 99;
-                }
-            }
-        }
-
-        
-
-        // current = position;
-        // if (d.dir == 'R') 
+        // Following for loop works
+        // for (int i = 0; i < d.amount; i++) 
         // {
-        //     position = position + d.amount;
-        //     position = position % 100; // Wrap around
-        // }
-        // else 
-        // {
-        //     if (d.amount > position) 
+        //     if (position == 0) 
         //     {
-        //         // We will pass
-        //         position = (position - (d.amount % 100) + 100) % 100;
+        //         count++;
+        //     }
+        //     if (d.dir == 'R') 
+        //     {
+        //         position++;
+        //         if (position > 99) 
+        //         {
+        //             position = 0;
+        //         }
         //     }
         //     else 
         //     {
-        //         position -= d.amount;
+        //         position--;
+        //         if (position < 0) 
+        //         {
+        //             position = 99;
+        //         }
         //     }
         // }
 
-        // // 5659 is too low
-        // // 5668 wrong 15:48
-        // // 6600 wrong 15:56
-        // 6386?
+        // Part two: 6386 - 51
 
-        // if (position < 0 || position > 99) 
-        // {
-        //     cout << "YOUR CODE IS WRONG" << endl;
-        //     return;
-        // }
+        count += checkZeroClicks(position, d);
 
-        // //cout << "The dial is rotated " << d.dir << d.amount << " to point at " << position;
-
-        // int zeroPasses = d.amount / 100;
-        // int remainder = d.amount % 100;
-        // int newValue = 0;
-        // if (d.dir == 'R')
-        // {
-        //     newValue = current + remainder;
-        // }
-        // else 
-        // {
-        //     newValue = current - remainder;
-        // }
-
-        // if (current == 0) {
-        //     count += zeroPasses;
-        // }
-        // else if (newValue <= 0) {
-        //     count += zeroPasses + 1;
-        // }
-        // else if (newValue >= 100) {
-        //     count += zeroPasses + 1;
-        // }
+        // Count new position
+        if (d.dir == 'R') 
+        {
+            position = position + d.amount;
+            position = position % 100; // Wrap around
+        }
+        else 
+        {
+           if (d.amount > position) 
+            {
+                position = (position - (d.amount % 100) + 100) % 100;
+            }
+            else 
+            {
+                position -= d.amount;
+            }
+        }
     }
 
     cout << "Part two: " << count << " - " << position << endl;
